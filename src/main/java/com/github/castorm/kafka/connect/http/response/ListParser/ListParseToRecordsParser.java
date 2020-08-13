80d210f46f6b738aa8675634395c6355f9207b58
Théo Schmidt <theo.schmidt@euranova.eu>
2020-08-13 12:10:15 +0200

    start package

---
+++ src/main/java/com/github/castorm/kafka/connect/http/response/ListParser/ListParseToRecordsParser.java
@@ -0,0 +1,75 @@
+package com.github.castorm.kafka.connect.http.response.ListParser;
+
+/*-
+ * #%L
+ * Kafka Connect HTTP
+ * %%
+ * Copyright (C) 2020 CastorM
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.castorm.kafka.connect.http.model.HttpResponse;
+import com.github.castorm.kafka.connect.http.response.spi.HttpResponseParser;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.apache.kafka.connect.errors.DataException;
+import org.apache.kafka.connect.source.SourceRecord;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+@RequiredArgsConstructor
+public class ListParseToRecordsParser implements HttpResponseParser {
+  private final Function<Map<String, ?>, ListParseToRecordsConfig> configFactory;
+  private final ObjectMapper objectMapper;
+  private String path;
+
+  public ListParseToRecordsParser() {
+    this(ListParseToRecordsConfig::new,new ObjectMapper());
+  }
+
+  @Override
+  public List<SourceRecord> parse(HttpResponse response) {
+        return ( ParseList(deserialize(response.getBody())),this.path)
+      }
+  @Override
+  public void configure(Map<String, ?> configs) {
+    ListParseToRecordsConfig config = configFactory.apply(configs);
+    path = config.getPath();
+  }
+
+  public static String requireString(Object value) {
+    if (!(value instanceof String)) {
+      throw new DataException(
+          "Only String objects supported for path, found: "
+              + nullSafeClassName(
+              value));
+    }
+    return (String) value;
+  }
+  private static String nullSafeClassName(Object x) {
+    return x == null ? "null" : x.getClass().getName();
+  }
+
+  @SneakyThrows(IOException.class)
+  private JsonNode deserialize(byte[] body) throws IOException {
+    return objectMapper.readTree(body);
+  }
+
+}
